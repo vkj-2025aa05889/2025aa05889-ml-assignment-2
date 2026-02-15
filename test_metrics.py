1c7efda3dee9514a39e234b2ba943504414acc9d
Kamakshi Jayanthi <2025aa05889@wilp.bits-pilani.ac.in>
2026-02-15 16:21:53 +0530

    Removed test files

--- test_metrics.py
+++
@@ -1,8 +0,0 @@
-from model.metrics import evaluate_classification_model
-
-y_true = [0, 1, 0, 1, 1]
-y_pred = [0, 1, 0, 0, 1]
-y_prob = [0.2, 0.8, 0.3, 0.4, 0.9]
-
-metrics = evaluate_classification_model(y_true, y_pred, y_prob)
-print(metrics)
